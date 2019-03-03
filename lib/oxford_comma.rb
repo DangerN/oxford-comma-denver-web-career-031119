11aedbad1c42d135757168ab340b4899170c1bd0
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 02:25:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   return array[0] while array.size < 2
   return "#{array[0]} and #{array[1]}" while array.size == 2
-  
+  last = array.pop
+  string = array.join(", ") << ", and #{last}"
+  string
 
 end
